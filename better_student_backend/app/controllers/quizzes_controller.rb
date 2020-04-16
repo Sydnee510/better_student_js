03b4be0d034d5c3f35e1f56f765aadd28b50bb55
sydnee510 <sbsydnee161416@gmail.com>
2020-04-16 02:48:30 -0700

    quiz controller render json

--- better_student_backend/app/controllers/quizzes_controller.rb
+++ better_student_backend/app/controllers/quizzes_controller.rb
@@ -1,21 +1,25 @@
 class QuizzesController < ApplicationController
     def index
         @quizzes = Quiz.all 
+        render json: @quizzes
     end
     def show 
         @quiz = Quiz.find(params[:id])
+        render json: @quiz
     end
     def create 
         @quiz = Quiz.create(quiz_params)
         #@quiz.save
+        render json: @quiz
     end
     def update 
         @quiz = Quiz.find(params[:id])
         @quiz.update(quiz_params)
+        render json: @quiz
     end
     def destroy
-        @quiz = Quiz.find(params[:id])
-        @quiz.destroy
+        @quiz = Quiz.find(params[:id]).destroy
+        render json: {id: @quiz.id}
     end
     private 
     def quiz_params
