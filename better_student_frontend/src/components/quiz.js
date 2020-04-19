654c53773ff3abeb4013be39f2f0898e39b0706c
sydnee510 <sbsydnee161416@gmail.com>
2020-04-19 00:08:39 -0700

    prints all quizzes to index

---
+++ better_student_frontend/src/components/quiz.js
@@ -0,0 +1,7 @@
+class Quiz {
+    constructor(quizJSON){
+        this.id = quizJSON.id
+        this.title = quizJSON.title
+        this.num_of_questions = quizJSON.num_of_questions
+    }
+}
--- better_student_frontend/src/components/quizzes.js
+++ better_student_frontend/src/components/quizzes.js
@@ -7,8 +7,8 @@ class Quizzes {
     }
     fetchAndLoadQuizzes(){
         this.adapter.getQuizzes().then(quizzes => {
-            quizzes.forEach(quiz => this.quizzes.push(quiz))
-        //return console.log(quizzes)
+            quizzes.forEach(quiz => this.quizzes.push(new Quiz(quiz)))
+            console.log(this.quizzes)
         })
         .then(() => {
             this.render()
@@ -16,8 +16,11 @@ class Quizzes {
 
     }
     render() {
+        //const quizzesString = console.log(quizzesString)
         const quizzesContainer = document.getElementById('quizzes-container')
-        quizzesContainer.innerHTML = "my quizzes here"
-        console.log('all quizzes', this.quizzes)
+       quizzesContainer.innerHTML = this.quizzes.map(quiz => `<li>${quiz.title}, ${quiz.num_of_questions}</li>`).join('')
+        //`${this.quizzes.map(quiz => <li>${quiz.title}</li>, <li>${quiz.num_of_questions}</li>)}`
+        //quizzesContainer.innerHTML = "my quizzes here"
+        //console.log('all quizzes', this.quizzes)
     }
 }
